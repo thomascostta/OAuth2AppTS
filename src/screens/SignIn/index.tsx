f97ccad97e0c2c3e4e036f3034c0132b4207af97
thomascostta <thomas.jeffcosta@gmail.com>
2022-07-08 14:42:31 -0300

    settings

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 import { useNavigation } from '@react-navigation/native';
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 import { Button } from '../../components/Button';
 import { SignInContent } from '../../components/SignInContent';
@@ -18,7 +22,18 @@ export function SignIn() {
   const navigation = useNavigation();
 
   async function handleSignIn() {
-    navigation.navigate('Profile');
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+    const { type, params } = await AuthSession
+      .startAsync({ authUrl }) as AuthResponse;
+
+    if (type === 'success') {
+      navigation.navigate('Profile', { token: params.access_token });
+    }
+
+
   }
 
   return (
